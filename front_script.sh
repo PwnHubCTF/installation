6d64840a4c76afea2e6a7b1aea7011b4d4a3f416
MathysV <56820126+Mathys-vende@users.noreply.github.com>
2023-01-31 13:43:33 +0100

    Update front_script.sh

--- front_script.sh
+++ front_script.sh
@@ -32,7 +32,7 @@ apt-get install -y  \
     gnupg \
     lsb-release > /dev/null 2>&1
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg >/dev/null | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg  --yes  > /dev/null 2>&1
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg  --yes  > /dev/null 2>&1
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
